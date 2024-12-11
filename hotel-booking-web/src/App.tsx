9eb2dcc6fc5abbef926b9be51e9c42a9e9417f6b
Oleg Matyokin <matyiokin2002@gmail.com>
2024-12-11 23:38:47 +0200

    Added web application

---
+++ hotel-booking-web/src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className="text-3xl flex justify-center items-center h-full ">
+      Hello world from front
+      <p>Hello world</p>
+    </div>
+  );
+}
+
+export default App;
